ce839827511379d0c36fa3bd31629ae98bdccb54
Jurijs Kolomijecs <jurikolo@yandex.com>
2021-11-14 22:19:43 +0200

    version 0.0.30, upgrade alicloud to 1.143.0

--- files/terraform/versions.tf
+++ files/terraform/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     alicloud = {
       source = "aliyun/alicloud"
-      version = "~> 1.142.0"
+      version = "~> 1.143.0"
     }
     hashicorp-alicloud = {
       source = "hashicorp/alicloud"
-      version = "~> 1.142.0"
+      version = "~> 1.143.0"
     }
     random = {
       source = "hashicorp/random"
