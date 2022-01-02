5c62f931f6ee9344a995c56b1558c72a0d769d81
Jurijs Kolomijecs <jurikolo@yandex.com>
2022-01-02 19:30:17 +0200

    version 0.0.39, upgrade AliCloud provider to version 1.150.0

--- files/terraform/versions.tf
+++ files/terraform/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     alicloud = {
       source = "aliyun/alicloud"
-      version = "~> 1.149.0"
+      version = "~> 1.150.0"
     }
     hashicorp-alicloud = {
       source = "hashicorp/alicloud"
-      version = "~> 1.149.0"
+      version = "~> 1.150.0"
     }
     random = {
       source = "hashicorp/random"
