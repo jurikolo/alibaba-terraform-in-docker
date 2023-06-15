9cde885e0805bbe1bf0a82fdf3ff67473779dac0
Jurijs Kolomijecs <jurikolo@yandex.com>
2023-06-15 09:08:51 +0300

    version 1.5.0, Terraform 1.5.0, AliCloud resource provider 1.206.0

--- files/terraform/versions.tf
+++ files/terraform/versions.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 1.4.6"
+  required_version = ">= 1.5.0"
   required_providers {
     alicloud = {
       source = "aliyun/alicloud"
-      version = "~> 1.203.0"
+      version = "~> 1.206.0"
     }
     hashicorp-alicloud = {
       source = "hashicorp/alicloud"
-      version = "~> 1.203.0"
+      version = "~> 1.206.0"
     }
     random = {
       source = "hashicorp/random"
